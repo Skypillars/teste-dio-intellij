c38dc71b977d97f3993ce1ca174b3c9c5ac28c56
skypillar <ph.panta@gmail.com>
2022-05-28 02:00:44 -0300

    first commit

---
+++ src/br/com/dio/testeintelij.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class testeintelij {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int s = 5;
+        System.out.println("hello world" + s);*/
+    }
+}
